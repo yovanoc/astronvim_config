f5db63e9425a469d9f731b2a2d4b8b9d54fee182
Christopher Yovanovitch <yovano_c@outlook.com>
2023-12-01 09:53:22 +0100

    some tweaks

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,14 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.colorscheme.tokyonight" },
 }
